e5d8449b5438c45dd9e13610ea6c9e5c6587d072
sravankumar1508 <sravankumar.n92@outlook.com>
2018-07-17 14:30:06 -0600

    data manipulated

---
+++ DML.sql
@@ -0,0 +1,120 @@
+USE nprsamples
+
+INSERT INTO ST
+VALUES ('CA', 'California')
+INSERT INTO ST
+VALUES ('CO', 'Colorodo')
+INSERT INTO ST
+VALUES ('UT', 'Utah')
+
+INSERT INTO Store
+VALUES ('1','101 E Ave','Denver','CO','80231','5103712000')
+
+INSERT INTO Customers
+VALUES ('000','Sravan Kumar','Nuthalapati','5103712011','s@gmail.com','3497 E Florida Ave','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('001','George','Cedar','1423256547','s@yahoo.com','3497 E Florida Ave','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('002','Arjun','','1234567895','','234 Parker Rd','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('003','Kelvin','Shrek','8764563476','kelvin@outlook.com','3497 E Florida Ave','Denver','CO','80098')
+INSERT INTO Customers
+VALUES ('004','Donald','North','7658768987','','3497 E Florida Ave','Fremont','CA','94538')
+INSERT INTO Customers
+VALUES ('005','Harsha','Kolla','1234567898','','654 Florida Ave','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('006','Mahesh','Potru','7654567645','','3495 California Blvd','St. George','UT','23456')
+INSERT INTO Customers
+VALUES ('007','Teja','Kolla','5103712011','teja@gmail.com','3497 E Florida Ave','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('008','Siri','Ravi','5103712011','ravi@ravi.com','3497 E Florida Ave','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('009','Monika','Ravi','4304301430','ravi@yahoo.com','2020 lawarence st','denver','co','80205')
+INSERT INTO Customers
+VALUES ('010','Vinod','Nuthalapati','5103712011','vinod@yahoo.com','3497 E Florida Ave','Denver','CO','80347')
+INSERT INTO Customers
+VALUES ('011','pranav','kumar','5105105100','kumar@yahoo.com','1039 s parker rd','denver','CO','80231')
+INSERT INTO Customers
+VALUES ('012','james','kumar','5105105101','kumarjames@yahoo.com','1037 s parker rd','denver','CO','80231')
+INSERT INTO Customers
+VALUES ('013','prem','kumar','5105115100','kumarprem@yahoo.com','1039 s parker rd','saltlakecity','UT','20121')
+INSERT INTO Customers
+VALUES ('014','TEDDY','kumar','5115115111','teddy@yahoo.com','1039 s teddy rd','denver','CO','80231')
+INSERT INTO Customers
+VALUES ('015','nuthala','kumar','5105105100','nuthan@yahoo.com','1039 s rodeo rd','denver','CO','80238')
+INSERT INTO Customers
+VALUES ('016','p','kumar','5165105109','pk@yahoo.com','1039 s parker rd','denver','CO','80231')
+INSERT INTO Customers
+VALUES ('017','jai','kumar','5105105100','jai@yahoo.com','1039 s ayodhya rd','gilory','CA','95203')
+INSERT INTO Customers
+VALUES ('018','LAVA','kumar','5105105100','LAV@yahoo.com','1039 s lanka rd','Los Angeles','CA','90100')
+INSERT INTO Customers
+VALUES ('019','KUSA','kumar','5105105100','KK@yahoo.com','1039 s Ramgopalvarma rd','boulder','CO','80239')
+
+
+INSERT INTO Employee
+VALUES ('100','pranav','kumar','5105105100','kumar@yahoo.com','1039 s parker rd','denver','CO','80231','123456789')
+INSERT INTO Employee
+VALUES ('101','mahesh','babu','5105105107','maheshbabu@yahoo.com',' 1020 noname rd','denver','CO','80231','000000001')
+INSERT INTO Employee
+VALUES ('102','Rehman','kumar','5105105122','RK@yahoo.com','1049 N parker rd','denver','CO','80231','000000020')
+INSERT INTO Employee
+VALUES ('103','pavan','kalyan','5115105100','Peekay@yahoo.com','1039 s cementry rd','denver','CO','80237','002000000')
+INSERT INTO Employee
+VALUES ('104','Tarak','Ram','5105225100','TARAK@yahoo.com','1039 s janthagarage rd','Aurora','CO','80247','011000000')
+INSERT INTO Employee
+VALUES ('105','mega','star','5105105100','Megastar@yahoo.com','1039 s King rd','denver','CO','80999','000009999')
+INSERT INTO Employee
+VALUES ('106','Samantha','Ruth','5105105100','samanatha@yahoo.com','1039 my heart rd','denver','CO','80233','143000000')
+INSERT INTO Employee
+VALUES ('107','rakul','preet','5105105100','preet@yahoo.com','1039 s xyz rd','boulder','CO','82312','002020200')
+
+INSERT INTO Category
+VALUES ('201','clothing')
+INSERT INTO Category
+VALUES ('202','shoes')
+INSERT INTO Category
+VALUES ('203','Bags and accesories')
+INSERT INTO Category
+VALUES ('204','Makeup')
+
+
+INSERT INTO Products
+VALUES ('301','201','Tedbaker shirt','M/P','145.00')
+INSERT INTO Products
+VALUES ('302','201','ZARA SKIRT','8','99.99')
+INSERT INTO Products
+VALUES ('303','202','gucci flower shoes','7.5','345.99')
+INSERT INTO Products
+VALUES ('304','202','aldo winter boots','7','45.00')
+INSERT INTO Products
+VALUES ('305','203','lavie phyton skin bag','one size','199.95')
+INSERT INTO Products
+VALUES ('306','203','aldo floral mini backpack','one size','49.95')
+INSERT INTO Products
+VALUES ('307','204','Sisley lipstick','one size','49.97')
+INSERT INTO Products
+VALUES ('308','204','NPR handsoap','large','19.95')
+
+INSERT INTO Orders
+VALUES ('401','009','2017/08/05 08:03:23 AM')
+INSERT INTO Orders
+VALUES ('402','004','2017/09/04 09:01:51 AM')
+
+INSERT INTO Orderline
+VALUES ('501','401','305','7')
+INSERT INTO Orderline
+VALUES ('502','402','306','6')
+
+INSERT INTO Appointment
+VALUES ('601','005','202','104','2017/09/07 10:00:00 AM')
+INSERT INTO Appointment
+VALUES ('602','008','203','103','2017/10/07 11:30:00 AM')
+
+
+
+
+
+
+
+
